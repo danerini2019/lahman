bedbb855578fbb8efe0977af74e9d5f233aa1d7a
danerini2019 <danerini@gmail.com>
2021-11-14 14:10:21 -0800

    added text

--- player_summary.sql
+++ player_summary.sql
@@ -1,23 +1,23 @@
--- SELECT 
--- 	p.playerID,
--- 	COUNT(DISTINCT f.POS) AS positions,
--- 	CONCAT(p.nameFirst, ' ', p.nameLast) AS name, 
--- 	SUM(AB) AS AB,
--- 	SUM(H) AS H,
--- 	SUM(2B) AS 2B,
--- 	SUM(3B) AS 3B,
--- 	SUM(BB) AS BB,
--- 	SUM(SO) AS SO,
--- 	SUM(b.HR) AS HR
--- FROM people p 
--- JOIN batting b 
--- 	ON p.playerID = b.playerID
--- JOIN fielding f
--- 	ON p.playerID = f.playerID 
--- WHERE b.yearID > 2000
--- GROUP BY p.playerID
--- ORDER BY AB DESC
--- AS table1
+SELECT 
+	p.playerID,
+	COUNT(DISTINCT f.POS) AS positions,
+	CONCAT(p.nameFirst, ' ', p.nameLast) AS name, 
+	SUM(AB) AS AB,
+	SUM(H) AS H,
+	SUM(2B) AS 2B,
+	SUM(3B) AS 3B,
+	SUM(BB) AS BB,
+	SUM(SO) AS SO,
+	SUM(b.HR) AS HR
+FROM people p 
+JOIN batting b 
+	ON p.playerID = b.playerID
+JOIN fielding f
+	ON p.playerID = f.playerID 
+WHERE b.yearID > 2000
+GROUP BY p.playerID
+ORDER BY AB DESC
+AS table1
 -- JOIN (
 SELECT * FROM fielding f
 LIMIT 100;
